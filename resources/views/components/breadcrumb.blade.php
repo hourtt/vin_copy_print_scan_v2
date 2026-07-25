31d9d969ec1714a8532378cc900ac312025f79c5
hourt <bunhourt79@gmail.com>
2026-07-25 15:49:44 +0700

    [UPDATE] Comment out back arrow icon in breadcrumb component

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -6,7 +6,7 @@
 @endphp
 
 <nav class="flex flex-wrap items-center text-sm font-['Kantumruy Pro',sans-serif] py-2 mb-6 gap-y-2">
-    @if ($showBack)
+    {{-- @if ($showBack)
         <a href="{{ route('breadcrumb.back', ['from' => $isTerminal ? 'terminal' : 'category']) }}"
            class="inline-flex items-center justify-center w-8 h-8 rounded-lg bg-gray-100 hover:bg-gray-200 text-gray-600 hover:text-gray-900 mr-3 transition-colors shrink-0"
            title="Go Back">
@@ -14,7 +14,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
             </svg>
         </a>
-    @endif
+    @endif --}}
 
     @foreach ($items as $item)
         <div class="flex items-center">
