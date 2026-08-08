d43d8b13dfa6481962cb6362878c37ee5a430820
hourt <bunhourt79@gmail.com>
2026-08-08 14:00:08 +0700

    [UPDATE] Refactor CartService resolution in AuthenticatedSessionController

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
-
+use App\Services\CartService;
 class AuthenticatedSessionController extends Controller
 {
     /**
@@ -26,7 +26,7 @@ class AuthenticatedSessionController extends Controller
     {
         $request->authenticate();
 
-        app(\App\Services\CartService::class)->mergeSessionToDb();
+        app(CartService::class)->mergeSessionToDb();
 
         $request->session()->regenerate();
 
