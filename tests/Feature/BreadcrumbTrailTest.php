b4eff166499b9062e3f170bbafb1cbcfc220104e
hourt <bunhourt79@gmail.com>
2026-08-22 15:20:30 +0700

    [UPDATE] Update breadcrumb navigation feature tests for catalog routes

--- tests/Feature/BreadcrumbTrailTest.php
+++ tests/Feature/BreadcrumbTrailTest.php
@@ -20,38 +20,15 @@ class BreadcrumbTrailTest extends TestCase
         $this->get(route('products.papers.index'));
         $this->assertEquals(['Printer', 'Paper'], array_column($trail->getStack(), 'label'));
 
-        // 3. Visit Cart icon
-        $responseCart = $this->get(route('cart.index'));
-        $responseCart->assertStatus(200);
-        $responseCart->assertSee('Dashboard');
-        $responseCart->assertSee('Paper');
-        $responseCart->assertSee('Checkout');
-
-        // 4. Back Arrow Click 1 (from terminal cart page)
-        $responseBack1 = $this->get(route('breadcrumb.back', ['from' => 'terminal']));
-        $responseBack1->assertRedirect(route('products.papers.index'));
-        $this->assertEquals(['Printer', 'Paper'], array_column($trail->getStack(), 'label'));
-
-        // 5. Back Arrow Click 2 (from Paper category page)
+        // 3. Back Arrow Click (from Paper category page)
         $responseBack2 = $this->get(route('breadcrumb.back', ['from' => 'category']));
         $responseBack2->assertRedirect(route('products.printers.index'));
         $this->assertEquals(['Printer'], array_column($trail->getStack(), 'label'));
 
-        // 6. Back Arrow Click 3 (from Printer category page)
+        // 4. Back Arrow Click (from Printer category page)
         $responseBack3 = $this->get(route('breadcrumb.back', ['from' => 'category']));
         $responseBack3->assertRedirect(route('dashboard'));
         $this->assertEquals([], array_column($trail->getStack(), 'label'));
-    }
-
-    public function test_example_b_direct_cart_icon_visit(): void
-    {
-        $trail = app(BreadcrumbTrail::class);
-        $trail->reset();
-
-        $response = $this->get(route('cart.index'));
-        $response->assertStatus(200);
-        $response->assertSee('Dashboard');
-        $response->assertSee('Shopping Cart');
     }
 
     public function test_example_c_product_catalog_clears_stack(): void
