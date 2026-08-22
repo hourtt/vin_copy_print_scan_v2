c6c01da724a88c51ffd6b8c49e363009ba81bc42
hourt <bunhourt79@gmail.com>
2026-08-22 11:27:12 +0700

    [REMOVE] Add migration to drop legacy ecommerce tables

---
+++ database/migrations/2026_08_22_050000_drop_legacy_ecommerce_tables.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('order_items');
+        Schema::dropIfExists('order_vouchers');
+        Schema::dropIfExists('orders');
+        Schema::dropIfExists('cart_items');
+        Schema::dropIfExists('carts');
+        Schema::dropIfExists('voucher_products');
+        Schema::dropIfExists('vouchers');
+        Schema::dropIfExists('shipping_methods');
+        Schema::enableForeignKeyConstraints();
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // One way migration
+    }
+};
